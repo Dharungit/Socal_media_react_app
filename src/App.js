949ea061614166852076f73c7a12124b374ded7f
Dharun <dharun123wizz@gmail.com>
2022-03-13 22:35:54 +0530

    changes for deploy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { getPosts } from "./actions/posts";
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
+import Home from "./components/Home/Home.js";
+import Form from "./components/form/form.js";
+import Auth from "./components/auth/auth";
+import PostDetails from "./components/postDetails/postDetails";
 
 function App() {
+  const [currentId, setCurrentId] = useState(0);
+  const dispatch = useDispatch();
+  const user = JSON.parse(localStorage.getItem("profile"));
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="font-pop">
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Navigate to="/posts" />} />
+          <Route
+            exact
+            path="/posts"
+            element={<Home setCurrentId={setCurrentId} />}
+          />
+          <Route
+            exact
+            path="/posts/search"
+            element={<Home setCurrentId={setCurrentId} />}
+          />
+          <Route exact path="/posts/:id" element={<PostDetails />} />
+          <Route
+            path="/form"
+            element={<Form currentId={currentId} setCurrentId={setCurrentId} />}
+          />
+          <Route
+            path="/auth"
+            element={!user ? <Auth /> : <Navigate to="/posts" />}
+          />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Home/Home.js
@@ -0,0 +1,25 @@
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+import Posts from "../Posts/Posts.js";
+import Header from "../Header/Header.js";
+
+function Home({ setCurrentId }) {
+  const navigate = useNavigate();
+  const user = JSON.parse(localStorage.getItem("profile"));
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/auth");
+    }
+  }, []);
+
+  return (
+    <div>
+      <Header />
+      <Posts setCurrentId={setCurrentId} />
+    </div>
+  );
+}
+
+export default Home;
