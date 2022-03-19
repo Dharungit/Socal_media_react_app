b293f8be574c07f5b84333b3f3bf9950b7ba1bbd
Dharun <dharun123wizz@gmail.com>
2022-03-19 17:54:17 +0530

    routes changes

--- src/App.js
+++ src/App.js
@@ -27,25 +27,39 @@ function App() {
     <div className="font-pop">
       <Router>
         <Routes>
-          <Route exact path="/" element={<Navigate to="/auth" />} />
+          {user && (
+            <>
+              <Route
+                exact
+                path="/posts"
+                element={<Home setCurrentId={setCurrentId} />}
+              />
+              <Route
+                exact
+                path="/posts/search"
+                element={<Home setCurrentId={setCurrentId} />}
+              />
+              <Route exact path="/posts/:id" element={<PostDetails />} />
+              <Route
+                path="/form"
+                element={
+                  <Form currentId={currentId} setCurrentId={setCurrentId} />
+                }
+              />
+            </>
+          )}
+
+          {!user && (
+            <Route
+              exact
+              path="/auth"
+              element={!user ? <Auth /> : <Navigate to="/posts" />}
+            />
+          )}
+
           <Route
-            exact
-            path="/posts"
-            element={<Home setCurrentId={setCurrentId} />}
-          />
-          <Route
-            exact
-            path="/posts/search"
-            element={<Home setCurrentId={setCurrentId} />}
-          />
-          <Route exact path="/posts/:id" element={<PostDetails />} />
-          <Route
-            path="/form"
-            element={<Form currentId={currentId} setCurrentId={setCurrentId} />}
-          />
-          <Route
-            path="/auth"
-            element={!user ? <Auth /> : <Navigate to="/posts" />}
+            path="*"
+            element={<Navigate to={!user ? "/auth" : "/posts"} />}
           />
         </Routes>
       </Router>
